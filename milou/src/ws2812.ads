901f677cfce382598eb1a67075dd6ebb86a64375
TristanBrouille <tristan.dhersin@gmail.com>
2026-01-15 17:19:46 +0100

    ameliration du driver

--- milou/src/ws2812.ads
+++ milou/src/ws2812.ads
@@ -1,32 +1,26 @@
+with Set_Pin_Stm32; use Set_Pin_Stm32;
+with Interfaces.STM32;
+
 package WS2812 is
-   type Bit is mod 2 ** 1; -- Définition du type Bit comme un entier modulaire sur 1 bit
-      type Color_Array is array (Positive range 1 .. 24) of Bit; -- Définition d'un tableau de 24 bits
+   type U8_T is range 0 .. 255 with Size => 8;
+   type WS2812_Device_T (Num_Leds : Positive) is private;
+   type Color_T is record
+      R : U8_T := 0;
+      G : U8_T := 0;
+      B : U8_T := 0;
+   end record;
+   procedure Init (Self : in out WS2812_Device_T);
+   procedure Set_GRB_At
+     (Self : in out WS2812_Device_T; Index : Positive; Color : Color_T);
+   procedure Set_GRB_All (Self : in out WS2812_Device_T; Color : Color_T);
+   procedure Show (Self : WS2812_Device_T);
+   procedure Clear (Self : WS2812_Device_T);
 
-      -- WS2812 nominal format is GRB, but observed bit mapping is GRB:
-      -- bits 1-8 = Blue, bits 9-16 = Red, bits 17-24 = Green
+private
+   type U1_T is mod 2**1 with Size => 1;
 
-      Green_Color : constant Color_Array := ( 
-         1, 1, 1, 1, 1, 1, 1, 1, -- Green: 8 bits
-         0, 0, 0, 0, 0, 0, 0, 0, -- Red: 8 bits
-         0, 0, 0, 0, 0, 0, 0, 0  -- Blue: 8 bits
-      );
-      
-      Red_Color : constant Color_Array := ( 
-         0, 0, 0, 0, 0, 0, 0, 0, -- Green: 8 bits
-         1, 1, 1, 1, 1, 1, 1, 1, -- Red: 8 bits
-         0, 0, 0, 0, 0, 0, 0, 0  -- Blue: 8 bits
-      );
-      
-      Blue_Color : constant Color_Array := ( 
-         0, 0, 0, 0, 0, 0, 0, 0, -- Green: 8 bits
-         0, 0, 0, 0, 0, 0, 0, 0, -- Red: 8 bits
-         1, 1, 1, 1, 1, 1, 1, 1  -- Blue: 8 bits
-      );
-      
-
-   procedure Init_LED;
-   procedure Set_LED (C : Color_Array);
-   procedure Latch_Period;
-
-   
-end WS2812;;
+   type Colors_T is array (Positive range <>) of Color_T;
+   type WS2812_Device_T (Num_Leds : Positive) is record
+      Leds : Colors_T (1 .. Num_Leds) := (others => (0, 0, 0));
+   end record;
+end WS2812;
